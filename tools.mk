2420b28d8c1ec4885e23e3b2cf76a32cff80c8b0
Botond Sipos <sbotond@ebi.ac.uk>
2013-08-13 16:14:33 +0100

    Fixes in tools.mk.

--- tools.mk
+++ tools.mk
@@ -9,6 +9,19 @@ LABELS	=$(BASE)/Info/dataset_labels.tab
 com:                                                                                                                                                           
 	git commit -a                                                                                                                                          
 
+# Runnables:
+RLSIM=$(BASE)/simulators/rlsim/rlsim
+EFFEST=$(BASE)/simulators/rlsim/tools/effest
+SIMNGS=$(BASE)/simulators/simNGS/bin/simNGS
+COV_CMP=$(BASE)/simulators/rlsim/tools/cov_cmp
+PB_PLOT=$(BASE)/simulators/rlsim/tools/pb_plot
+PLOT_RLSIM=$(BASE)/simulators/rlsim/tools/plot_rlsim_report
+VER_LOG  = $(BASE)/Info/soft_versions.log
+
+# simNGS runfile:
+RUNFILE_DIR=$(BASE)/simulators/simNGS/data
+RUNFILE=$(RUNFILE_DIR)/s_3_4x.runfile
+
 # Build tools:
 
 $(SIMNGS):                                                                                                                                                     
@@ -37,19 +50,3 @@ gr:
 	-p `find $(BASE)/Bias -name meta_pb_sim*.pk` -r $(GLOB_REP) -bc $(BASE)/Replicate/*/log/cov_cmp.pk \
 	-bp $(BASE)/Replicate/*/log/meta_pb.pk
 
-# Runnables:
-RLSIM=$(BASE)/simulators/rlsim/rlsim
-EFFEST=$(BASE)/simulators/rlsim/tools/effest
-SIMNGS=$(BASE)/simulators/simNGS/bin/simNGS
-COV_CMP=$(BASE)/simulators/rlsim/tools/cov_cmp
-PB_PLOT=$(BASE)/simulators/rlsim/tools/pb_plot
-PLOT_RLSIM=$(BASE)/simulators/rlsim/tools/plot_rlsim_report
-KMER_FREQS = $(BASE)/scripts/kmer_freqs
-KMER_FREQS_COR = $(BASE)/scripts/kmer_freqs_cor
-MEAN_GC = $(BASE)/scripts/mean_gc
-MEAN_GC_COR = $(BASE)/scripts/mean_gc_cor
-VER_LOG  = $(BASE)/Info/soft_versions.log
-
-# simNGS runfile:
-RUNFILE_DIR=$(BASE)/simulators/simNGS/data
-RUNFILE=$(RUNFILE_DIR)/s_3_4x.runfile
